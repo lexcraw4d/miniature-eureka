34f3ed5ed48fcb4a431800bd487b53758ccc80cd
Lexie Crawford <lexcraw4d@gmail.com>
2021-06-06 17:16:52 -0400

    connected express server and routes to frontend endpoints

--- Develop/routes/index.js
+++ Develop/routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const database = require('../db/db.json');
-const path = require('path')
+const path = require('path');
 
 // Rest API CRUD Paradigm
 // Get    - R ead   - Gets data from backend
@@ -10,16 +10,9 @@ const path = require('path')
 // Delete - D elete - Erase one entity from your backend.
 
 // '/' GET
-router.route('/').get(function (req, res) {
+router.route('/api').get(function (req, res) {
 	res.send(`Note Taker API version 0.0.1`);
+	// res.render('index', { title: 'Hey', message: 'Hello there!' });
 });
 
-//links html static
-router.get('/notes', (req, res) => {
-	res.sendFile(path.join(__dirname, '../public/notes.html'));
-});
-
-router.get('*', (req, res) => {
-	res.sendFile(path.join(__dirname, '../public/index.html'));
-});
 module.exports = router;
--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -6,11 +6,12 @@ const fs = require('fs');
 // let notesId = 2;
 
 router
-	.route('/notes')
+	.route('/api/notes')
 	// Returns all notes
 	.get((req, res) => {
 		res.send(database);
 	})
+
 	// Creates new note
 	.post((req, res) => {
 		const newNote = req.body;
@@ -28,25 +29,51 @@ router
 	});
 
 router
-	.route('/notes/:id')
+	.route('/api/notes/:id')
+	// Returns single note by id.
 	.get((req, res) => {
 		const id = req.params.id;
 		const note = database.find((el) => el.id === Number(id));
+		if (!note) {
+			// Note was not found with that id.
+			return res.status(404).send('Entity not found');
+		}
 		res.send(note);
 	})
+
+	// Update single note (by id).
 	.put((req, res) => {
 		const id = req.params.id;
 		const update = req.body;
 		const noteIndex = database.map((el) => el.id).indexOf(Number(id));
 
 		if (noteIndex < 0) {
-			return res.send('Entity not found');
+			// Note was not found with that id.
+			return res.status(404).send('Entity not found');
 		}
 
+		// Update note fields.
 		database[noteIndex].title = update.title;
 		database[noteIndex].content = update.content;
+
 		fs.writeFileSync('db/db.json', JSON.stringify(database, null, 2));
 		res.send(database[noteIndex]);
+	})
+
+	// Delete single note (by id).
+	.delete((req, res) => {
+		const id = req.params.id;
+		// Check if db has such id.
+		const noteIndex = database.map((el) => el.id).indexOf(Number(id));
+
+		if (noteIndex < 0) {
+			// Note was not found with that id.
+			return res.status(404).send('Entity not found');
+		}
+
+		database.splice(noteIndex, 1);
+		fs.writeFileSync('db/db.json', JSON.stringify(database, null, 2));
+		res.send('Ok');
 	});
 
 module.exports = router;
