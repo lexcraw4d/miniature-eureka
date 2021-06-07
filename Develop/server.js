ca6260b4de4db38575ad5e6ce02355544bcc3093
Lexie Crawford <lexcraw4d@gmail.com>
2021-06-06 20:18:54 -0400

    Frontend connected to backend server, functionality success v0.0.1

--- Develop/server.js
+++ Develop/server.js
@@ -1,23 +1,21 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
-const path = require ('path')
-
-
+const path = require('path');
 
 // App Routes
 const indexRouter = require('./routes/index');
 const notesRouter = require('./routes/notes');
+
+// App Configurations
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use(express.urlencoded({extended: true}));
-
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.set('view engine', 'pug');
 
+// Routes prefixed by '/api'
 app.use(indexRouter);
-
 app.use(notesRouter);
-
 app.listen(PORT, (req, res) => {
 	console.log(`Express App listening on ${PORT}: http://localhost:${PORT}`);
 });
